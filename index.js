94b057926bddaee1eee247d50c6d71087de22b78
Boban Jankovic <boban0092@gmail.com>
2020-03-05 22:17:19 +0100

    Gallery fixing

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ $('.popup-gallery').magnificPopup({
     image: {
         tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
         titleSrc: function(item) {
-            return item.el.attr('title') + '<small>by Boban Janković</small>';
+            return "Kentaur Apartmani created by: " + '<small>by Boban Janković</small>';
         }
     }
 });
+
