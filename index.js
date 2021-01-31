cdce802c2593952b542f0eddaab41474914c42e4
Boban <boban0092@gmail.com>
2021-01-31 20:32:08 +0100

    Banner pwa install IOS

--- index.js
+++ index.js
@@ -43,6 +43,22 @@ if('serviceWorker' in navigator){
 
 
 
+// Detects if device is on iOS
+const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+    showPwaBanner();
+}
+
+function showPwaBanner(){
+    $(".pwa--container").show();
+}
 
 
 
